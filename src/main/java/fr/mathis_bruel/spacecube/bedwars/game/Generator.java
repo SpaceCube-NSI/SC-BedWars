572d9f5b3f7d42b32340f1f0e05e64f9fd302d1d
AzionMC <70230064+MathisBruel@users.noreply.github.com>
2023-01-31 23:49:32 +0100

    Generator Manager update

--- src/main/java/fr/mathis_bruel/spacecube/bedwars/game/Generator.java
+++ src/main/java/fr/mathis_bruel/spacecube/bedwars/game/Generator.java
@@ -2,15 +2,75 @@ package fr.mathis_bruel.spacecube.bedwars.game;
 
 import org.bukkit.Material;
 
-import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public enum Generator {
     //  generatorType(Matrerial.type, Levels[0,1,2,3,..])
-    iron(Material.IRON_INGOT, Arrays.asList(5,2.5,2)),
-    gold(Material.GOLD_INGOT, Arrays.asList(5,2.5,2)),
-    diamond(Material.DIAMOND, Arrays.asList(10,5,2.5)),
-    emerald(Material.EMERALD, Arrays.asList(20,15,13.5));
+    iron(Material.IRON_INGOT, Arrays.asList(5.0, 2.5, 2.0)),
+    gold(Material.GOLD_INGOT, Arrays.asList(5.0, 2.5, 2.0)),
+    diamond(Material.DIAMOND, Arrays.asList(10.0, 5.0, 2.5)),
+    emerald(Material.EMERALD, Arrays.asList(20.0, 15.0, 13.5));
+
+    public Material material;
+    public List<Double> levels;
+
+    Generator(Material material, List<Double> levels) {
+        this.material = material;
+        this.levels = levels;
+    }
+
+    public Material getMaterial() {
+        return material;
+    }
+
+    public List<Double> getLevels() {
+        return levels;
+    }
+
+    public double getLevel(int level) {
+        return levels.get(level);
+    }
+
+    public int getLevelSize() {
+        return levels.size();
+    }
+
+    public static Generator getGenerator(Material material) {
+        for (Generator generator : values()) {
+            if (generator.getMaterial() == material) {
+                return generator;
+            }
+        }
+        return null;
+    }
+
+    public static Generator getGenerator(String name) {
+        for (Generator generator : values()) {
+            if (generator.name().equalsIgnoreCase(name)) {
+                return generator;
+            }
+        }
+        return null;
+    }
+
+    public static boolean isGenerator(Material material) {
+        for (Generator generator : values()) {
+            if (generator.getMaterial() == material) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean isGenerator(String name) {
+        for (Generator generator : values()) {
+            if (generator.name().equalsIgnoreCase(name)) {
+                return true;
+            }
+        }
+        return false;
+    }
 
 
 
