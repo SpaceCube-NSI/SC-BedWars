e9731cde6b77173611d5649d66555b628ed34e1d
AzionMC <70230064+MathisBruel@users.noreply.github.com>
2023-01-31 23:11:31 +0100

    Initial commit

---
+++ src/main/java/fr/mathis_bruel/spacecube/bedwars/Main.java
@@ -0,0 +1,18 @@
+package fr.mathis_bruel.spacecube.bedwars;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
